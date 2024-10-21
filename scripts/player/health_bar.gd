5f1be5e3a0769c95859a1775e14cb4636dd90866
helo1223 <hajnalmarkhelo@gmail.com>
2024-10-21 03:06:25 +0200

    cleanup

--- scripts/player/health_bar.gd
+++ scripts/player/health_bar.gd
@@ -4,7 +4,5 @@ extends ProgressBar
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    if is_instance_of(player, Player) and not is_instance_of(player, NPC):
-        pass
     var max_hp_with_shield = player.max_health + player.shield
     self.value = (player.health / max_hp_with_shield) * 100
